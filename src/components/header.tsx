16d492e5ea610b78c05605873d028df3c597d298
Alan Sunny <73838186+nalaso@users.noreply.github.com>
2024-10-21 14:51:04 +0530

    Update eslint configuration and modify header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,16 +11,14 @@ const Header = () => {
 		<>
 			<div className="w-full bg-white flex justify-between items-center p-4">
 				<div className="flex space-x-2">
-					<Button onClick={() => router.push("/")} variant={"ghost"} className="text-xl font-bold">
+					<Button onClick={() => router.push("/")} variant={"flat"} className="text-xl font-bold">
 						StoryAI
 					</Button>
-					<Button onClick={() => router.push("/explore")} variant={"bordered"} className="text-xl font-semibold">Explore</Button>
+					<Button onClick={() => router.push("/explore")} variant={"flat"} className="text-xl font-bold">Explore</Button>
 				</div>
-				{/* <div className="flex space-x-2 items-center">
-					{status === "unauthenticated" && (
-						<Button onClick={toggleAuth} variant="flat">Sign In</Button>
-					)}
-				</div> */}
+				<div className="flex space-x-2 items-center">
+					<Button onClick={()=>router.push("https://github.com/nalaso/storyai")}className="text-xl font-bold" variant="flat">Github</Button>
+				</div>
 			</div>
 		</>
 	);
